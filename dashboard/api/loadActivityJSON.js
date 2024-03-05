519ffe60d07ce06545f1fdb2ac40838f51476701
Josh Cherian <jcherian92@gmail.com>
2024-03-05 10:57:52 -0500

    debug

--- dashboard/api/loadActivityJSON.js
+++ dashboard/api/loadActivityJSON.js
@@ -3,9 +3,12 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), "data", "activities.json");
+  console.log("hello?");
+  console.log(process.cwd(), "data", "activities.json");
+  console.log(path.join("data", "activities.json"));
+  const file = path.join("data", "activities.json");
   const stringified = readFileSync(file, "utf8");
   const data = JSON.parse(stringified)["data"];
-  console.log("hello?");
+
   res.send(data);
 }
