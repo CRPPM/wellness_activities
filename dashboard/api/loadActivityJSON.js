007e1bcc9db520279d84773622bad4806472c4a1
Josh Cherian <jcherian92@gmail.com>
2024-03-12 13:41:15 -0400

    moving debugs

--- dashboard/api/loadActivityJSON.js
+++ dashboard/api/loadActivityJSON.js
@@ -7,6 +7,9 @@ export default function handler(req, res) {
   const file = path.join(process.cwd(), "dashboard", "data", "activities.json");
   const stringified = readFileSync(file, "utf8");
   let data = JSON.parse(stringified)["data"];
+  console.log(data[0].EmoBreaths);
+  console.log(data[0].EmoBreathsFreqW);
+  console.log(data[0].EmoBreathsTimeW);
 
   switch (goal) {
     case "Sleep":
@@ -65,8 +68,6 @@ export default function handler(req, res) {
       });
       break;
   }
-  console.log(data[0].EmoBreaths);
-  console.log(data[0].EmoBreathsFreqW);
-  console.log(data[0].EmoBreathsTimeW);
+
   res.send(data);
 }
