f3bb3550209261230f2f596cb1123bf7a19b818f
Josh Cherian <jcherian92@gmail.com>
2024-03-26 17:12:49 -0400

    Added in MHSG and PHSG demos

--- Python Scripts/calculate_rbo.py
+++ Python Scripts/calculate_rbo.py
@@ -17,10 +17,18 @@ df = df[df['StartDate'].notna()]
 df['ageG'] = df['age'].apply(lambda x: 1 if x >= 18 and x <= 29 else (2 if x >= 30 and x <= 49 else 3))
 
 goals = {'Sleep': 'Sleep', 'Phys': 'Physical Health', 'Emo': 'Emotional Health', 'Product':'Productivity', 'Social': 'Social Wellness'}
-demos = {'GenderB': 'Gender', 'sexorB': 'Sexual Orientation',
-         'incomeB': 'Income', 'ageG': 'Age',
-         'MHSG': 'Mental Health Diagnosis', 'PHSG': 'Physical Health Diagnosis',
-         'WearYN': 'Wearable Use', 'WellnessAppG': 'Meditation App Use'}
+demos_overall = {'GenderB': 'Gender', 'sexorB': 'Sexual Orientation',
+                 'incomeB': 'Income', 'ageG': 'Age',
+                 'MHSG': 'Mental Health Diagnosis', 'PHSG': 'Physical Health Diagnosis',
+                 'WearYN': 'Wearable Use', 'WellnessAppG': 'Meditation App Use'}
+
+demos_PHSG = {'MedWeightSelf': 'Unhealthy Weight', 'MedMuscleSelf': 'Musculoskeletal Disorder',
+              'MedGastroSelf': 'Gastrointestinal Disorder', 'MedCancerSelf': 'Cancer',
+              'MedDiabetesSelf': 'Diabetes', 'MedCardioSelf': 'Cardiovascular', 'MedLungSelf': 'Lung',
+              'MedNeuroSelf': 'Neuro'}
+demos_MHSG = {'MedDepSelf': 'Depression', 'MedPTSDSelf': 'PTSD', 'MedAnxSelf': 'Anxiety',
+              'MedBipolarSelf': 'Bipolar', 'MedAutismSelf': 'Autism', 'MedSocialSelf': 'Social',
+              'MedOCDSelf': 'OCD', 'MedPanicSelf': 'Panic Attacks'}
 
 def calc_rbo(l1,l2,p):
     """ 
@@ -69,7 +77,7 @@ def calc_rbo(l1,l2,p):
     return rbo_ext
 
 def get_top_activities(df_goal, goal):
-    act_cols = [col for col in df_goal if col.startswith(g) and '_' not in col and col != g + 'Goal']
+    act_cols = [col for col in df_goal if col.startswith(goal) and '_' not in col and col != goal + 'Goal']
     act_cols = act_cols[0:25]
 
     df_acts = df_goal[act_cols]
@@ -79,40 +87,73 @@ def get_top_activities(df_goal, goal):
     return list(df_acts.count().sort_values(ascending=False).index.values[0:10])
 
 
-df_graph = pd.DataFrame(columns=['Goal', 'Demo', 'RBO'])
+def plot_RBO(demos, remove_other_demos=False):
+    df_graph = pd.DataFrame(columns=['Goal', 'Demo', 'RBO', 'Label'])
+    min_RBO = 2
+    max_RBO = 0
+    for g in goals.keys():
+        print('Goal:', g)
+        df_goal = df.loc[df[g + 'Goal'] != 0]
 
-for g in goals.keys():
-    print('Goal:', g)
-    df_goal = df.loc[df[g + 'Goal'] != 0]
+        for d in demos.keys():
+            if not remove_other_demos:
+                if df[d].dtype == 'category':
+                    A = df[d].cat.categories[0]
+                    B = df[d].cat.categories[1]
+                elif d == 'ageG':
+                    A = 1
+                    B = 3
+                elif d == 'MHSG' or d == 'PHSG':
+                    A = 1
+                    B = 0
+                else:
+                    print('Breaks on ', d)
+                    exit()
 
-    for d in demos.keys():
-        if df[d].dtype == 'category':
-            A = df[d].cat.categories[0]
-            B = df[d].cat.categories[1]
-        elif d == 'ageG':
-            A = 1
-            B = 3
-        elif d == 'MHSG' or d == 'PHSG':
-            A = 1
-            B = 0
-        else:
-            print(d)
-            print('breaks!')
-            exit()
+                df_A = df_goal.loc[df_goal[d] == A]
+                df_B = df_goal.loc[df_goal[d] == B]
 
-        df_A = df_goal.loc[df_goal[d] == A]
-        df_B = df_goal.loc[df_goal[d] == B]
+            else:
+                if not (df_goal[d].cat.categories == [0.0, 'Self']).all:
+                    print('Breaks on ', d)
+                    exit()
+                df_A = df_goal.loc[df_goal[d] == 0.0]
+                for demo in demos.keys():
+                    df_A = df_A.loc[df_A[demo] == 0.0]
 
-        A_list = get_top_activities(df_A, g)
-        B_list = get_top_activities(df_B, g)
+                df_B = df_goal.loc[df_goal[d] == 'Self']
 
-        new_record = pd.DataFrame([{'Goal':goals[g], 'Demo':demos[d], 'RBO':calc_rbo(A_list, B_list, 0.98)}])
-        df_graph = pd.concat([df_graph, new_record], ignore_index=True)
+            A_list = get_top_activities(df_A, g)
+            B_list = get_top_activities(df_B, g)
+            
+            rbo = calc_rbo(A_list, B_list, 0.98)
+            max_RBO = max(max_RBO, rbo)
+            min_RBO = min(min_RBO, rbo)
+            
+            new_record = pd.DataFrame([{'Goal':goals[g], 'Demo':demos[d], 'RBO':rbo, 'Label': len(df_B)}])
+            df_graph = pd.concat([df_graph, new_record], ignore_index=True)
 
-ax = sns.barplot(x='Demo', y='RBO', hue='Goal', data=df_graph)
-ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
-plt.xlabel('Demographics')
-plt.ylim([0, 1])
-sns.despine()
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.3), ncol=5)
-plt.show()
+    print('')
+    print('Max RBO:', max_RBO)
+    print('Min RBO:', min_RBO)
+
+    ax = sns.barplot(x='Demo', y='RBO', hue='Goal', data=df_graph)
+    if remove_other_demos:
+        for i, bar in enumerate(ax.patches[0:len(df_graph)]):
+            bar_value = df_graph['Label'].iloc[i]
+            text_x = bar.get_x() + bar.get_width() / 2
+            text_y = bar.get_y() + bar.get_height()
+
+            ax.text(text_x, text_y, bar_value, ha='center', va='bottom', size=10)
+
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.xlabel('Demographics')
+    plt.ylim([0, 1])
+    sns.despine()
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25), ncol=5)
+    plt.show()
+
+if __name__ == "__main__":
+    # remove_other_demos should be True if MHSG or PHSG, otherwise False
+    plot_RBO(demos_overall, remove_other_demos=False)
+
